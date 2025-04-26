a16e08a16d782673f65374802bca8abdb32de7bb
Valeria Akulinina <lerrun@gmail.com>
2025-04-25 21:50:41 -0600

    feat: implement jobs ui  (#7)
    * feat: display jobs for techs
    
    * chore: update supabase ssr version
    
    * feat: update ui

--- apps/web/src/utils/supabase/server.ts
+++ apps/web/src/utils/supabase/server.ts
@@ -1,33 +1,28 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { Database } from '@/types/database.types'
+import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export const createClient = async () => {
   const cookieStore = await cookies()
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          const cookie = cookieStore.get(name)
-          return cookie?.value
+        getAll() {
+          return cookieStore.getAll()
         },
-        set(name: string, value: string, options: CookieOptions) {
+        setAll(cookiesToSet) {
           try {
-            cookieStore.set(name, value, options)
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options),
+            )
           } catch (error) {
             // Handle cookie setting error
           }
         },
-        remove(name: string, options: CookieOptions) {
-          try {
-            cookieStore.set(name, '', { ...options, maxAge: 0 })
-          } catch (error) {
-            // Handle cookie removal error
-          }
-        },
       },
-    }
+    },
   )
-} 
+}
